7e6fe8d29febaba16f0b3b4b777cec67311891db
Gavin Gassmann <gassmann.gavin@gmail.com>
2018-11-20 16:28:47 -0600

    Switch to roboto/opensans

--- src/frontend/GlobalStyle.ts
+++ src/frontend/GlobalStyle.ts
@@ -4,6 +4,10 @@ import { getColor } from './Theme';
 export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    font-family: 'Open Sans', sans-serif;
+  }
+  h1,h2,h3,h4,h5,h6 {
+    font-family: 'Roboto', sans-serif;
   }
   a {
     color: ${getColor({type: 'font'})};
